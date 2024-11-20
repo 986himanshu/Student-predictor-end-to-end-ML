c011c59060e7f6873f5d4f1e3802a76f7f663d6a
Himanshu Sharma <986himanshusharma@gmail.com>
2024-11-20 12:30:26 +0530

    Run on latest version of python 3.12.7

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ version='0.0.1',
 author='Krish',
 author_email='krishnaik06@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=get_requirements('requirements.txt'),
+python_requires='>=3.11'
 
 )
